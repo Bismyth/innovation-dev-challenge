f4b08260ab1f673187f7c0bb6a895b8e14c6a46f
Adam Bardsley <adam.bardsley@innovation-i.com.au>
2022-12-19 15:56:12 +0800

    Fix

--- backend/init_backend.ps1
+++ backend/init_backend.ps1
@@ -1,9 +1,9 @@
-python3 -m venv ./venv
+python -m venv ./venv
 .\venv\Scripts\activate
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
-python3 create_db.py
+python create_db.py
 
 alembic revision --autogenerate -m "Init"  
 alembic upgrade head
